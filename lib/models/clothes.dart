1e0ea47b74974f73821a073d70c42c630f20245d
mbayzhanova <moldirbaijanova03@gmail.com>
2022-03-14 21:40:43 +0600

    clothes

--- lib/models/clothes.dart
+++ lib/models/clothes.dart
@@ -11,22 +11,36 @@ class Clothes {
     return [
       Clothes(
           'Блузка с узором \nMACROCANNAGE',
-          'Блузка Dior',
+          'Dior',
           '\$2186.10',
           'assets/images/1.png',
           ['assets/images/2.png', 'assets/images/3.png']),
       Clothes(
-          'Мини-платье \n La Robe Saudade',
-          'Платье JACQUEMUS',
+          '     Мини-платье \n La Robe Saudade',
+          'JACQUEMUS',
           '\$654,84',
           'assets/images/jc.png',
           ['assets/images/jc1.png', 'assets/images/jc2.png']),
       Clothes(
           'Пиджак с вырезами',
-          'Пиджак GIVENCHY',
+          'GIVENCHY',
           '\$2019,79',
           'assets/images/giv1.png',
-          ['assets/images/giv1.png', 'assets/images/giv1.png'])
+          ['assets/images/giv1.png', 'assets/images/giv1.png']),
+      Clothes(
+          'Трикотажный пуловер\nс молнией на четверть',
+          'Ralph Lauren',
+          '\$98.50',
+          'assets/images/prl1.png',
+          ['assets/images/prl2.png', 'assets/images/prl3.png']),
+      Clothes(
+          'Шерстяной бомбер\n   прямого кроя',
+          'HUGO BOSS',
+          '\$755,82',
+          'assets/images/hb1.png',
+          ['assets/images/hb2.png', 'assets/images/hb3.png']),
+      Clothes('Свитшот «Gucci Band»\n для детей 3–12 лет', 'Gucci', '\$329,13',
+          'assets/images/guccib1.png', ['assets/images/guccib2.png'])
     ];
   }
 }
