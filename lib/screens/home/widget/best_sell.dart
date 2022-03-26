c7646b526b5792a0cf55e21ccad019b42e5a9b58
mbayzhanova <moldirbaijanova03@gmail.com>
2022-03-26 23:52:13 +0600

    best, foto, fav

--- lib/screens/home/widget/best_sell.dart
+++ lib/screens/home/widget/best_sell.dart
@@ -5,7 +5,26 @@ class BestSell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: [CategoriesList('лучший из продаж')]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CategoriesList('Лучшие из продажи'),
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Stack(
+              children: [
+                Row(
+                  children: [],
+                ),
+                Positioned(
+                  child: Icon(Icons.favorite),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
