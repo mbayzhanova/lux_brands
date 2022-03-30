6e805bc8c5f783f2d1acfc53f299f719e05b7519
mbayzhanova <moldirbaijanova03@gmail.com>
2022-03-30 11:16:17 +0600

    30,03

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -5,15 +5,26 @@ import 'package:lux_brands/screens/home/widget/search_input.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final bottomList = ['home', 'menu', 'heart', 'user'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [CustomAppBar(), SearchInput(), NewArrival(), BestSell()],
-          ),
-        ));
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [CustomAppBar(), SearchInput(), NewArrival(), BestSell()],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: bottomList
+            .map((e) => BottomNavigationBarItem(
+                label: e, icon: Image.asset('assets/icons/$e.png', width: 25)))
+            .toList(),
+      ),
+    );
   }
 }
